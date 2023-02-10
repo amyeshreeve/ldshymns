06c762976c1b2c065f9b4d1f9e62bfd603b8861d
Amy Shreeve <79295223+amyeshreeve@users.noreply.github.com>
2023-02-10 13:57:42 -0600

    trouble with pip

--- modeller.py
+++ modeller.py
@@ -6,8 +6,7 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 import csv
 import pandas as pd
-import gensim
-from gensim.utils import simple_preprocess
+import spacey
 
 filelocation = input("Input file location: ")
 name = input("Desired file name with .csv: ")
@@ -44,3 +43,11 @@ with open('old_hymn_cleaned.txt', 'w', encoding = 'latin-1') as f:
 with open('new_hymn_cleaned.txt', 'w') as f:
   for text in newhymns['Content'].tolist():
     f.write(str(text) + '\n')
+
+#Even later
+
+with open('new_hymn_cleaned.txt', 'r') as file:
+    newhymns = file.read()
+
+with open('old_hymn_cleaned.txt', 'r') as file:
+    oldhymns = file.read()
